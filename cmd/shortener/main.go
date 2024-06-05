646e277a9b2254c7381f3a1efcfb7eeca73ec68b
User Egor <egoeik112@gmail.com>
2024-06-05 18:00:02 +0300

    cmd/shortener(main.go): Add func PostHandler() and GetHandler()

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+
+	w.WriteHeader(http.StatusCreated)
+
+	data := []byte("http://localhost:8080/EwHXdJfB")
+
+	_, err := w.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain")
+
+	w.WriteHeader(http.StatusTemporaryRedirect)
+
+	data := []byte("https://practicum.yandex.ru/")
+
+	_, err := w.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", PostHandler)
+	mux.HandleFunc("/EwHXdJfB", GetHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
