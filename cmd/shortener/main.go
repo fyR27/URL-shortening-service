8490b1d4300780c2202b0150f9bc86bd5bd67691
Egor <130695351+fyR27@users.noreply.github.com>
2024-06-10 15:49:12 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,49 +2,16 @@ package main
 
 import (
 	"net/http"
+
+	app "github.com/fyR27/URL-shortening-service/internal/app"
 )
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain")
-
-	w.WriteHeader(http.StatusCreated)
-
-	data := []byte("http://localhost:8080/EwHXdJfB")
-
-	_, err := w.Write(data)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain")
-
-	w.WriteHeader(http.StatusTemporaryRedirect)
-
-	data := []byte("https://practicum.yandex.ru/")
-
-	_, err := w.Write(data)
-	if err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", PostHandler)
-	mux.HandleFunc("/EwHXdJfB", GetHandler)
+	mux.HandleFunc("/", app.PostHandle)
+	mux.HandleFunc("/EwHXdJfB", app.GetHandle)
 
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		panic(err)
 	}
 }
