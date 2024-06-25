d0841a2935c2b2a41c4f563dbfcad9c53011063b
Egor <130695351+fyR27@users.noreply.github.com>
2024-06-25 15:47:03 +0300

    Update handler.go

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -6,36 +6,34 @@ import (
 )
 
 func PostHandle(w http.ResponseWriter, r *http.Request) {
-	var s Storage
+	var p ParserURL
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 
-	data := []byte("http://localhost:8080" + string(s.AddNewURL(body)))
+	data := []byte("localhost:8080/" + p.AddNewURL(body))
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
 
 	if _, err := w.Write(data); err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
 func GetHandle(w http.ResponseWriter, r *http.Request) {
-	var s Storage
+	var p ParserURL
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	data := s.FindAddr("/EwHXdJfB")
-
-	w.Header().Set("Content-Type", "text/plain")
+	data := p.FindAddr(p.ID)
 
 	http.Redirect(w, r, data, http.StatusTemporaryRedirect)
 }
