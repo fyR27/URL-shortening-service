40929503409c55d6ad574c646e99aadbae6bb02c
Egor <130695351+fyR27@users.noreply.github.com>
2024-06-26 14:44:25 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.PostHandle)
-	mux.HandleFunc("/{id}", app.GetHandle)
+	mux.HandleFunc("GET /{id}", app.GetHandle)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		panic(err)
