5b772d77bec8ec6875ac4cff481445177bdca608
Egor <130695351+fyR27@users.noreply.github.com>
2024-07-22 16:17:19 +0300

    Update handler.go

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -7,38 +7,33 @@ import (
 
 func MakePostHandle(store *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
+
 		body, err := io.ReadAll(r.Body)
-		if err != nil {
+		if err != nil || string(body) == "" {
 			w.WriteHeader(http.StatusBadRequest)
-		}
+		} else {
 
-		data := []byte("http://localhost:8080/" + store.AddNewURL(body))
+			data := []byte("http://localhost:8080/" + store.AddNewURL(body))
 
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusCreated)
+			w.Header().Set("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusCreated)
 
-		if _, err := w.Write(data); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			if _, err := w.Write(data); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+			}
 		}
 	}
 }
 
 func MakeGetHandle(store *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
 
 		data := store.FindAddr(r.URL.Path[1:])
 		if data == "Bad id" {
 			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			http.Redirect(w, r, data, http.StatusTemporaryRedirect)
 		}
 
-		http.Redirect(w, r, data, http.StatusTemporaryRedirect)
 	}
 }
