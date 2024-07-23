d8c28807c1190fa103e3223b475dcb2d2d6bdcca
Egor <130695351+fyR27@users.noreply.github.com>
2024-07-23 07:02:13 +0300

    Update handler.go

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -5,19 +5,13 @@ import (
 	"net/http"
 )
 
-func init() {
-	NewStorage()
-}
-
 func MakePostHandle(store *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil || string(body) == "" {
 			w.WriteHeader(http.StatusBadRequest)
 			return
-		}
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
 		}
 
 		data := []byte("http://localhost:8080/" + store.AddNewURL(body))
@@ -32,19 +26,12 @@ func MakePostHandle(store *Storage) http.HandlerFunc {
 }
 
 func MakeGetHandle(store *Storage) http.HandlerFunc {
-
 	return func(w http.ResponseWriter, r *http.Request) {
-		var s Storage
-		if r.Method != http.MethodGet {
+		data := store.FindAddr(r.URL.Path[1:])
+		if data == "Bad id" {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-
-		data := s.FindAddr(r.URL.Path)
-		if data == "Bad id" {
-			w.WriteHeader(http.StatusBadRequest)
-		}
-
 		http.Redirect(w, r, data, http.StatusTemporaryRedirect)
 	}
 }
