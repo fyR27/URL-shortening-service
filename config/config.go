65770a48f5b2f31067f73f44f7c56a463bcb1a41
Egor <130695351+fyR27@users.noreply.github.com>
2024-09-06 17:30:33 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"flag"
-
-	"github.com/google/uuid"
 )
 
 type Config struct {
@@ -14,12 +12,12 @@ type Config struct {
 func NewConfig() *Config {
 	return &Config{
 		Host: ":8080",
-		URL:  uuid.NewString(),
+		URL:  "http://localhost",
 	}
 }
 
 func ParseFlags(c *Config) {
 	flag.StringVar(&c.Host, "a", ":8080", "address to run server ")
-	flag.StringVar(&c.URL, "b", uuid.NewString(), "url to get base url")
+	flag.StringVar(&c.URL, "b", "http://localhost", "url to get base url")
 	flag.Parse()
 }
