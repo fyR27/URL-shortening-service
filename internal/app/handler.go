ff8d3494fbae891e4c93069118de50d2151c98b1
Egor <130695351+fyR27@users.noreply.github.com>
2024-09-09 07:07:03 +0300

    Update handler.go

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -1,9 +1,18 @@
 package app
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
+
+func ParsedBaseURL(host, url, body string) string {
+	if url != "" && url != "http://localhost" || ((url[len(url)-1] >= 48 && url[len(url)-1] <= 57) && url != "http://localhost") {
+		return "http://" + url + "/" + body
+	} else {
+		return url + host + "/" + body
+	}
+}
 
 func MakePostHandle(store *Storage, host, url string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -14,25 +23,13 @@ func MakePostHandle(store *Storage, host, url string) http.HandlerFunc {
 			return
 		}
 
-		id := store.AddNewURL(body)
-
-		// Формирование итогового сокращённого URL
-		parsedBaseURL, err := url.Parse(baseURL)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// Установка пути в сокращённый URL
-		parsedBaseURL.Path = path.Join(parsedBaseURL.Path, id)
-
-		// Формирование итоговой строки URL
-		shortURL := parsedBaseURL.String()
-
-		// Отправка ответа
+		parsedBody := store.AddNewURL(body)
+		data := ParsedBaseURL(host, url, parsedBody)
+		fmt.Println(data)
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
-		if _, err := w.Write([]byte(shortURL)); err != nil {
+
+		if _, err := w.Write([]byte(data)); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
