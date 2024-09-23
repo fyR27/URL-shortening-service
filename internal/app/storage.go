dea390228eb75e1129d972a82dba4440997499f2
Egor <130695351+fyR27@users.noreply.github.com>
2024-09-23 16:24:13 +0300

    Update storage.go

--- internal/app/storage.go
+++ internal/app/storage.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/google/uuid"
+import (
+	"github.com/fyR27/URL-shortening-service/config"
+	"github.com/google/uuid"
+)
 
 type ParsedURL struct {
 	ID  string
@@ -17,13 +20,21 @@ func NewStore() *Storage {
 	}
 }
 
-func (s *Storage) AddNewURL(body []byte) string {
+func ValidURL(c *config.Config) string {
+	if c.URL[len(c.URL)-1] >= 48 && c.URL[len(c.URL)-1] <= 57 {
+		return c.URL + "/"
+	} else {
+		return c.URL + c.Host + "/"
+	}
+}
+
+func (s *Storage) AddNewURL(body []byte, c *config.Config) string {
 	newURL := &ParsedURL{
 		ID:  uuid.NewString(),
 		URL: string(body[:]),
 	}
 	s.store[newURL.ID] = newURL.URL
-	return newURL.ID
+	return ValidURL(c) + newURL.ID
 }
 
 func (s *Storage) FindAddr(url string) string {
